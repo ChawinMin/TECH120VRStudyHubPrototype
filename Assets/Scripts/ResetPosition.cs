996c0cf0705e90beb8ddc95ba15d7cbce095e12e
Oki <63143341+Oki634@users.noreply.github.com>
2024-08-07 10:41:36 -0400

    Added quit function

--- Assets/Scripts/ResetPosition.cs
+++ Assets/Scripts/ResetPosition.cs
@@ -5,13 +5,17 @@ using UnityEngine.SceneManagement;
 
 public class ResetPosition : MonoBehaviour
 {
-
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.R))
+        if (Input.GetKeyDown(KeyCode.R))
         {
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
         }
     }
 }
